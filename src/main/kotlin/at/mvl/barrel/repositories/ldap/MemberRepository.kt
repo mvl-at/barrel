66f5f9742bbd7a8a3a8933c41fad20447a7b865d
Richard Stöckl <richard.stoeckl@aon.at>
2021-10-01 19:33:40 +0200

    Add kdoc to MemberRepository

--- src/main/kotlin/at/mvl/barrel/repositories/ldap/MemberRepository.kt
+++ src/main/kotlin/at/mvl/barrel/repositories/ldap/MemberRepository.kt
@@ -9,13 +9,29 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource
 import org.springframework.data.rest.core.annotation.RestResource
 import org.springframework.security.access.prepost.PreAuthorize
 
+/**
+ * @author Richard Stöckl
+ *
+ * Repository for accessing members.
+ * Suited for management and therefore required authentication for all methods.
+ * Exposes confidential user data such as addresses, emails and phone numbers.
+ */
 @RepositoryRestResource
 @Tag(name = "Member Repository", description = "Repository for all MVL members with extended data support")
 @PreAuthorize("hasRole(@roleMap.roles().memberManager)")
 interface MemberRepository : LdapRepository<Member> {
 
+    /**
+     * Find a member by its username.
+     * @param username the username to search for
+     * @return the [Member] with the corresponding username or 'null' if such member does not exist
+     */
     fun findByUsername(username: String): Member?
 
+    /**
+     * Find all members which are part of the music corps.
+     * @return a list of all members which are part of the music corps
+     */
     @Query(base = "ou=Musik,ou=Mitglieder", value = "(objectClass=inetOrgPerson)")
     @RestResource(path = "/musicians", exported = true, description = Description("All musicians of this musikverein"))
     fun findAllMusicians(): List<Member>
