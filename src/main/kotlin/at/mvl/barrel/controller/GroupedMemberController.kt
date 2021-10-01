1eecef6faa862d304214e6c90cf0f845f1d87742
Richard Stöckl <richard.stoeckl@aon.at>
2021-10-01 17:31:56 +0200

    Add kdoc to GroupedMemberController

--- src/main/kotlin/at/mvl/barrel/controller/GroupedMemberController.kt
+++ src/main/kotlin/at/mvl/barrel/controller/GroupedMemberController.kt
@@ -1,5 +1,6 @@
 package at.mvl.barrel.controller
 
+import at.mvl.barrel.model.ExternalMember
 import at.mvl.barrel.model.Register
 import at.mvl.barrel.repositories.ldap.ExternalMemberRepository
 import at.mvl.barrel.repositories.ldap.RegisterRepository
@@ -13,8 +14,13 @@ import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.server.ResponseStatusException
 import javax.naming.ldap.LdapName
 
+/**
+ * @author Richard Stöckl
+ *
+ * Controller used for grouping tasks for members.
+ */
 @RestController
-@RequestMapping(path = ["\${spring.data.rest.base-path}/groupedmembers"])
+@RequestMapping("\${spring.data.rest.base-path}/groupedmembers")
 class GroupedMemberController(
     @Autowired val registerRepository: RegisterRepository,
     @Autowired val memberRepository: ExternalMemberRepository
@@ -22,6 +28,13 @@ class GroupedMemberController(
 
     private val logger: Logger = LoggerFactory.getLogger(GroupedMemberController::class.java)
 
+    /**
+     * Group members into their registers and sort both registers and members.
+     * Only members which are listed are affected.
+     * The sorting methods used are [ExternalMember.compareTo] for members and [Register.compareTo] for registers.
+     *
+     * @return the list of registers with the members
+     */
     @GetMapping("")
     fun getMembersGroupedByRegister(): List<Register> {
         logger.trace("getMembersGroupedByRegister()")
@@ -29,8 +42,9 @@ class GroupedMemberController(
         registers.forEach { r ->
             r.members = r.allMembers.map { mid ->
                 val dn = LdapName(mid)
-                val member = memberRepository.findByUsername(dn.getRdn(dn.size() - 1).value.toString())
-                if (member == null) {
+                val member =
+                    memberRepository.findByUsername(dn.getRdn(dn.size() - 1).value.toString()) // used to extract uid={0} from the full DN
+                if (member == null) { // this should only happen when referential integrity within the LDAP is violated
                     logger.error("Cannot find member with dn={}", mid)
                     throw ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Unable to find dn")
                 }
