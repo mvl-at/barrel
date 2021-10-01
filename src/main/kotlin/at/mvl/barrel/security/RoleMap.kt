abea9bf8ec746b6f445119457cfe617842b16247
Richard Stöckl <richard.stoeckl@aon.at>
2021-10-02 00:07:31 +0200

    Add kdoc to the role map

--- src/main/kotlin/at/mvl/barrel/security/RoleMap.kt
+++ src/main/kotlin/at/mvl/barrel/security/RoleMap.kt
@@ -4,9 +4,16 @@ import at.mvl.barrel.configuration.BarrelConfigurationProperties
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
+/**
+ * A simple wrapper for the role mappings.
+ */
 @Component
 class RoleMap(@Autowired private val barrelConfigurationProperties: BarrelConfigurationProperties) {
 
+    /**
+     * Return the role map from the properties.
+     * @return the role map
+     */
     fun roles(): BarrelConfigurationProperties.RoleMappingConfiguration {
         return barrelConfigurationProperties.roles
     }
