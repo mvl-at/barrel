eae70f73fbf8b1dfcc8acfb2199c07ec9148d2ce
Richard Stöckl <richard.stoeckl@aon.at>
2021-10-01 20:05:19 +0200

    Add kdoc to GenreRepository

--- src/main/kotlin/at/mvl/barrel/repositories/GenreRepository.kt
+++ src/main/kotlin/at/mvl/barrel/repositories/GenreRepository.kt
@@ -3,6 +3,13 @@ package at.mvl.barrel.repositories
 import at.mvl.barrel.model.Genre
 import org.springframework.data.repository.CrudRepository
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
+import org.springframework.security.access.prepost.PreAuthorize
 
+/**
+ * @author Richard Stöckl
+ *
+ * Repository for managing genres.
+ */
 @RepositoryRestResource
+@PreAuthorize("hasRole(@roleMap.roles().archive)")
 interface GenreRepository : CrudRepository<Genre, Long>
