cad3986192dbf285d04e969c3f08804d13d3c25f
Richard Stöckl <richard.stoeckl@aon.at>
2021-10-02 00:04:01 +0200

    Add kdoc to JwtTokenService

--- src/main/kotlin/at/mvl/barrel/security/JwtTokenService.kt
+++ src/main/kotlin/at/mvl/barrel/security/JwtTokenService.kt
@@ -15,6 +15,14 @@ import java.util.*
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
+/**
+ * @author Richard Stöckl
+ * A service which generates and parses JWT tokens.
+ *
+ * @param barrelConfigurationProperties the properties for setting the JWT parameters
+ * @property userDetailsService the [UserDetailsService] to fetch user information on token parsing
+ * @property jwtConfig a shorthand for the JWT properties
+ */
 class JwtTokenService(
     barrelConfigurationProperties: BarrelConfigurationProperties,
     private val userDetailsService: UserDetailsService
@@ -25,17 +33,34 @@ class JwtTokenService(
     private val jwtConfig: BarrelConfigurationProperties.SecurityConfiguration.JwtConfiguration =
         barrelConfigurationProperties.security.jwt
 
+    /**
+     * Write a token generated from the [auth] to the [response] Authorization Header.
+     * @param auth the authentication to use
+     * @param response where the token should be written to
+     */
     fun addTokenToHeader(auth: Authentication, response: HttpServletResponse) {
         logger.trace("addTokenToHeader({},{})")
         response.setHeader(HttpHeaders.AUTHORIZATION, "${jwtConfig.prefix} ${generateToken(auth, false)}")
     }
 
+    /**
+     * Write a renewal token generated from the [auth] to the [response] body.
+     * @param auth the authentication to use
+     * @param response where the token should be written to
+     */
     fun addRenewalTokenToBody(auth: Authentication, response: HttpServletResponse) {
         logger.trace("addRenewalTokenToBody({},{})", auth, response)
         response.writer.println(generateToken(auth, true))
         response.setHeader(HttpHeaders.CONTENT_TYPE, "application/jwt")
     }
 
+    /**
+     * Parse the authentication token from a request and return it if it is a normal token.
+     * @see parseAuthenticationFromRequest
+     *
+     * @param request the request from which to parse the token
+     * @return an authentication if the [request] contains a valid token and 'null' otherwise
+     */
     fun authenticationFromToken(request: HttpServletRequest): Authentication? {
         logger.trace("authenticationFromToken({})", request)
         val pair = parseAuthenticationFromRequest(request) ?: return null
@@ -46,6 +71,13 @@ class JwtTokenService(
         return pair.first
     }
 
+    /**
+     * Parse the authentication token from a request and return it if it is a renewal token.
+     * @see parseAuthenticationFromRequest
+     *
+     * @param request the request from which to parse the renewal token
+     * @return an authentication if the [request] contains a valid renewal token and 'null' otherwise
+     */
     fun authenticationFromRenewalToken(request: HttpServletRequest): Authentication? {
         logger.trace("authenticationFromRenewalToken({})", request)
         val pair = parseAuthenticationFromRequest(request) ?: return null
@@ -56,10 +88,22 @@ class JwtTokenService(
         return pair.first
     }
 
+    /**
+     * Test if the provided request contains an Authorization attempt.
+     * @param request the request to test
+     * @return 'true' if the [request] contains an Authorization attempt and 'false' otherwise
+     */
     fun hasAuthorization(request: HttpServletRequest): Boolean {
         return request.getHeader(HttpHeaders.AUTHORIZATION) != null
     }
 
+    /**
+     * Generate a token for a given [Authentication] as configured in the provided [jwtConfig].
+     *
+     * @param auth the authentication to use
+     * @param renewal whether the resulting token should be a renewal token or not
+     * @return the resulting JWT token
+     */
     private fun generateToken(auth: Authentication, renewal: Boolean): String {
         logger.trace("generateToken({},{})", auth, renewal)
         val inst = Instant.now()
@@ -74,6 +118,21 @@ class JwtTokenService(
             .compact()
     }
 
+    /**
+     * Parse a token from a request using the Authorization Header.
+     * This includes both, normal and renewal tokens.
+     * Iff the parsed token is a renewal token, the second parameter of the returned [Pair] will be 'true' and 'false' otherwise.
+     *
+     * The first parameter of the returned [Pair] is the [Authentication] for the username provided in the token.
+     * The result will be 'null' in case of:
+     *  * the value of the Authorization Header does not start with 'Bearer '
+     *  * the token is expired
+     *  * the provided value is no token
+     *  * the signature of the token is invalid
+     *  * the value after 'Bearer ' is empty
+     *
+     * @return the result of this parse attempt
+     */
     fun parseAuthenticationFromRequest(request: HttpServletRequest): Pair<Authentication, Boolean>? {
         logger.trace("parseAuthenticationFromRequest({},{})", request, request)
         val token = request.getHeader(HttpHeaders.AUTHORIZATION)
