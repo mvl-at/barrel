19b264e83c5f8ab75dd14b0213ead1c35c601953
Richard Stöckl <richard.stoeckl@aon.at>
2021-10-01 20:05:37 +0200

    Add kdoc to GradeRepository

--- src/main/kotlin/at/mvl/barrel/repositories/GradeRepository.kt
+++ src/main/kotlin/at/mvl/barrel/repositories/GradeRepository.kt
@@ -3,6 +3,13 @@ package at.mvl.barrel.repositories
 import at.mvl.barrel.model.Grade
 import org.springframework.data.repository.CrudRepository
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
+import org.springframework.security.access.prepost.PreAuthorize
 
+/**
+ * @author Richard Stöckl
+ *
+ * Repository for managing grades.
+ */
 @RepositoryRestResource
+@PreAuthorize("hasRole(@roleMap.roles().archive)")
 interface GradeRepository : CrudRepository<Grade, Long>
